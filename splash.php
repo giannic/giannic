415868d3da5bd9757f4b657940f1c4a7c5aa42c5
Gianni Chen <giannikchen@gmail.com>
2013-07-06 23:16:11 -0700

    removing splash screen

---
+++ splash.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="google-site-verification"
+        content="C6_wOCVRZCOck1tRKN7tWxzKq0t2rBLxWFDEUMgrS5c"/>
+    <meta name="Description"
+        content="Hi! I'm Gianni. I am currently an undergrad in Digital Media Design at the University of Pennsylvania.">
+    <title>Gianni Chen</title>
+    <link rel="stylesheet" href="css/splash.css" type="text/css">
+    <?php
+        include 'config.php';
+        if ($env == 'prod') {
+            include 'php/analytics.php';
+        }
+    ?>
+</head>
+
+<body>
+<section class="index">
+    <a class="logo" href="index.php">G</a>
+</section>
+</body>
+
+</html>
