f1a4437f99c9ddb1974bccac42387597242f667c
Gianni Chen <giannikchen@gmail.com>
2013-07-06 23:58:10 -0700

    update profiles

--- profiles.php
+++ profiles.php
@@ -29,26 +29,62 @@
                     <tr>
                         <td class="badge">
                             <a target="_blank"
-                                href="http://www.linkedin.com/in/giannic">
-                                <img src="img/linkedin.png"
-                                    width="80" height="80" border="0"
-                                    alt="LinkedIn"/>
+                                href="http://medium.com/@giannichen">
+                                <img src="https://s3.amazonaws.com/obvious/logos/M_128_black.png"
+                                width="64" height="64"
+                                alt="Medium"/>
                             </a>
                         </td>
+
                         <td class="badge">
                             <a target="_blank"
-                                href="http://www.github.com/giannic">
-                                <img src="img/octocat.png"
+                                href="http://github.com/giannic">
+                                <img src="https://a248.e.akamai.net/assets.github.com/images/modules/logos_page/GitHub-Mark.png"
                                     width="80" height="80" border="0"
                                     alt="Github"/>
                             </a>
                         </td>
+
                         <td class="badge">
                             <a target="_blank"
                                 href="http://pinterest.com/giannic">
                                 <img src="http://passets-cdn.pinterest.com/images/big-p-button.png"
                                 width="64" height="64"
                                 alt="Pinterest"/>
+                            </a>
+                        </td>
+
+                        <td class="badge">
+                            <a target="_blank"
+                                href="http://twitter.com/giannichen">
+                                <img src="https://abs.twimg.com/a/1372759208/images/resources/twitter-bird-white-on-blue.png"
+                                width="64" height="64"
+                                alt="Twitter"/>
+                            </a>
+                        </td>
+
+                        <td class="badge">
+                            <a target="_blank"
+                                href="http://linkedin.com/in/giannic">
+                                <img src="http://developer.linkedin.com/sites/default/files/LinkedIn_Logo60px.png"
+                                    width="81" height="64" border="0"
+                                    alt="LinkedIn"/>
+                            </a>
+                        </td>
+                        <td class="badge">
+                            <a target="_blank"
+                                href="http://facebook.com/in/giannic">
+                                <img src="https://www.facebookbrand.com/img/assets/asset.f.logo.lg.png"
+                                    width="64" height="64" border="0"
+                                    alt="Facebook"/>
+                            </a>
+                        </td>
+                        <td class="badge">
+                            <a target="_blank"
+                                href="http://google.com/profiles/giannikchen">
+                                <img src="https://ssl.gstatic.com/images/icons/gplus-64.png"
+                                    width="64" height="64" border="0"
+                                    alt="Google"/>
                             </a>
                         </td>
                     </tr>
