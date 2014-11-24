48be3c8447a52adaa54998c5229f28cae9656ec5
gianni <giannikchen@gmail.com>
2014-11-23 17:43:21 -0800

    update info

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
     <title>about gianni</title>
     <meta charset="utf-8">
     <meta name="description"
-        content="Gianni, undergraduate in Digital Media Design at the University of Pennsylvania.">
+        content="Gianni, software engineer at Medium.">
     <meta name="google-site-verification"
         content="C6_wOCVRZCOck1tRKN7tWxzKq0t2rBLxWFDEUMgrS5c"/>
     <meta name="p:domain_verify" content="bfc9fbb599b3e78fa77875f50ae02ad1"/>
     <?php
         include 'config.php';
         include 'php/functions.php';
-        
+
         meta();
         stylesheets();
         less();
@@ -33,13 +33,18 @@
 
         <section class="content">
             <article>
-                <h2 class="section-title">about</h2>
+                <!--hax-->
+                <h3 class="section-title">&nbsp;</h3>
                 <p class="entry">
-                    Hi! I am Gianni. I am a senior in Digital Media Design as well as a Master's student in Computer Graphics and Game Technology at the University of Pennsylvania. I have an interest in coding, but I do art casually as well. I love clean code and minimalistic designs. My hobbies include table tennis, badminton and sketching.
-                    <br/><br/>
-                    A lot of my free time is spent working on personal and group projects as well as customizing my computer. I am most familiar with vim, but used to be an emacs guy. I have spent countless hours battling the irregularities of the unix environment on Macs, but still enjoy the strings it has drawn between the Terminal, Photoshop and Maya.
-                    <br/><br/>
-                    Please take a look around my site. All comments are welcome!
+                    Hi! My name is Gianni.
+                    <br>
+                    I am a software engineer
+                    <br>
+                    at <a href="http://medium.com">Medium</a>. I studied
+                    <br>
+                    <a href="http://dmd.upenn.edu">Digital Media Design</a> at
+                    <br>
+                    The University of Pennsylvania.
                 </p>
             </article>
         </section>
